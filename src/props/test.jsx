f0f9e7435a16e0959b7e554f548ed1af57390579
Jeneliya12 <gurungjeneliya@gmail.com>
2024-08-24 23:59:27 -0400

    learn usestate for input form

--- src/props/test.jsx
+++ src/props/test.jsx
@@ -1,18 +1,23 @@
 import React, { useState } from "react";
 function Test() {
-  const [isVisible, setIsVisible] = useState(true);
+  const [inputValue, setInputValue] = useState("");
 
-  const toggleVisibility = () => {
-    setIsVisible(!isVisible);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("inputValue", inputValue);
+  };
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
   };
 
   return (
     <>
-      <button onClick={toggleVisibility}>
-        {isVisible ? "Hide" : "Show"}
-        Text
-      </button>
-      {isVisible && <p>Text</p>}
+      <form onSubmit={handleSubmit}>
+        <label>Input</label>
+        <input type="text" value={inputValue} onChange={handleChange} />
+        <button type="submit">Submit</button>
+      </form>
     </>
   );
 }
